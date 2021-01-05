5f956ea69444fe430e62cac744c8e514d682f237
ninohero <ninohero@muzocalo.com>
2021-01-05 14:28:38 -0500

    added identify-square util func and tested

--- src/utils/fill-grid/index.ts
+++ src/utils/fill-grid/index.ts
@@ -34,6 +34,11 @@ function fillGrid(grid: GRID) {
     for (let value of numbers) {
       if (!isInRow({ grid, row, value })) {
         if (!isInCol({ col, grid, value})) {
+          const square = [
+            [0,0,0],
+            [0,0,0],
+            [0,0,0]
+          ]
           // is it not in the grid square?
           // ......... if this is the case
 
